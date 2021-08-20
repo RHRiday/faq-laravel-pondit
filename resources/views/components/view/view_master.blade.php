cf6e00041b3586bdbb2db1e9776a18d18617a36e
RHRiday <riffatriday@gmail.com>
2021-08-20 16:48:19 +0600

    v2.0

---
+++ resources/views/components/view/view_master.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{{$title}}</title>
+    <link rel="icon" href="{{ asset('/view/img/favicon.ico') }}">
+
+    {{--Styles file included--}}
+    @include('view.layouts.partials.style')
+
+
+</head>
+
+<body class="full_body">
+
+<div class="container">
+
+    {{--Header file included--}}
+    @include('view.layouts.partials.header')
+
+
+    <div class="row tob_bar">
+        <div class="col-md-12 text-center mb-4 mt-4">
+            <h1 class="my_h1"> <span class="type"></span></h1>
+        </div>
+        <div class="col-md-6 mx-auto">
+            <input type="text" id="search" class="form-control search_form"
+                   placeholder="&#xf002; Search for questions...">
+        </div>
+    </div>
+
+    <div class="row my_content">
+        <div class="col-md-12">
+            <div class="container">
+                <div class="row" style="margin-top: 30px">
+
+                    {{--Sidebar included--}}
+                    @include('view.layouts.partials.sidebar')
+
+                    {{--Main Component Body--}}
+                    {{$slot}}
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{--Footer included--}}
+    @include('view.layouts.partials.footer')
+
+</div>
+
+{{--Scripts file included--}}
+@include('view.layouts.partials.script')
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\FaqController;
+use App\Http\Controllers\Admin\FAQController;
+use App\Http\Controllers\View\ViewController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//=============View site=================
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ViewController::class, 'index']);
 
-Auth::routes();
+Route::get('/search-faq/{search}', [ViewController::class, 'faq_search']);
 
-Route::resource('/faq', FaqController::class);
+//=============Admin site=================
+
+Route::get('/home', [FAQController::class, 'index']);
+
+Route::post('/add_faq', [FAQController::class, 'faq_add']);
+
+Route::post('/update_faq', [FAQController::class, 'faq_update']);
+
+Route::get('/manage_faq', [FAQController::class, 'faq_manage']);
+
+Route::get('/edit_faq/{id}', [FAQController::class, 'faq_edit']);
+
+Route::get('/delete_faq/{id}', [FAQController::class, 'faq_delete']);
+
+Route::get('/unpublished_faq/{id}', [FAQController::class, 'unpublished_faq']);
+
+Route::get('/published_faq/{id}', [FAQController::class, 'published_faq']);
+
+Route::get('/priority-set/', [FAQController::class, 'priority_set']);
